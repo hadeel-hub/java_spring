2fe4d7d031cc1d51386f265f78185a0a85f71e8c
hadeel-hub <hadeela_k2050@yahoo.com>
2022-11-01 11:58:17 +0200

    test

---
+++ Hoopers/src/main/java/com/coding/spring/HoopersApplication.java
@@ -0,0 +1,13 @@
+package com.coding.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoopersApplication.class, args);
+	}
+
+}
